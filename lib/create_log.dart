cc224b49d32d79570618da515e0309360cf067f2
Gudan Makumbane <1846579@students.wits.ac.za>
2022-04-06 13:27:39 +0200

    push

---
+++ lib/create_log.dart
@@ -0,0 +1,104 @@
+
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+import 'join_log.dart';
+
+
+class createL extends StatefulWidget{
+
+  const createL({Key? key}) : super(key: key);
+
+  @override
+  _createL createState() => _createL();
+}
+
+class _createL extends State<createL>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.green,
+        title: const Text("Create New Log"),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.only( top: /*MediaQuery.of(context).size.height * 0.15*/ 0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize : MainAxisSize.max,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.80,
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText:"Enter Log Name"
+                      ),
+                    ),
+
+                    SizedBox(height: 30),
+
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      height: 50,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(30),),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            primary: Colors.green,
+                          ),
+                            onPressed: () {
+                              //Navigator.push(context, MaterialPageRoute(builder: (context){return const NameOfThePage();},),);
+                            },
+
+                            child: const Text("SAVE")),
+                      ),
+                    ),//sizedbox
+
+                    SizedBox(height: 30),
+
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      height: 50,
+                      child: ClipRRect(
+                          borderRadius: const BorderRadius.all(Radius.circular(30),),
+                          child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.green,
+                              ),
+                              onPressed: () {
+                                Navigator.push(context, MaterialPageRoute(builder: (context){return const joinL();},),);
+                              },
+                              child: const Text("Add Items"),
+                             // style: ElevatedButton.styleFrom(primary: Colors.black26,textStyle: const TextStyle(color: Colors.black,fontSize: 20,))
+                          )
+                      ),
+                    ),//sizedbox
+
+                  ],//children
+
+                ),//Column
+
+
+              ),//sizedbox
+
+            ],//children
+
+          ),//column
+
+        ),//center
+
+      ),//padding
+
+
+
+    );
+  }
+}
---
+++ lib/join_log.dart
@@ -0,0 +1,93 @@
+
+// ignore_for_file: camel_case_types
+
+import 'package:flutter/material.dart';
+
+
+
+class joinL extends StatefulWidget{
+
+  const joinL({Key? key}) : super(key: key);
+
+  @override
+  _joinL createState() => _joinL();
+}
+
+class _joinL extends State<joinL>{
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor:Colors.green,
+        title: const Text("JOIN LOG"),
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.only( top: /*MediaQuery.of(context).size.height * 0.15*/ 0),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize : MainAxisSize.max,
+            children: [
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.80,
+                child: Column(
+                  children: [
+                    SizedBox(height: 10),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText:"Enter Log Name"
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    const TextField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText:"Enter Log ID/Link"
+                      ),
+                    ),
+
+                    SizedBox(height: 30),
+
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.80,
+                      height: 50,
+                      child: ClipRRect(
+                        borderRadius: const BorderRadius.all(Radius.circular(30),),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                            ),
+                            onPressed: () {
+                              //Navigator.push(context, MaterialPageRoute(builder: (context){return const NameOfThePage();},),);
+                            },
+
+                            child: const Text("JOIN")),
+                      ),
+                    ),//sizedbox
+
+
+
+                  ],//children
+
+                ),//Column
+
+
+              ),//sizedbox
+
+            ],//children
+
+          ),//column
+
+        ),//center
+
+      ),//padding
+
+
+
+    );
+  }
+}
