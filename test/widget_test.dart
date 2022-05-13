fa0ea2314e43e2ca7bbb7888da054c1003071ac7
Gudani55 <69737478+Gudani55@users.noreply.github.com>
2022-05-13 22:44:50 +0200

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,20 +10,24 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:harvest/main.dart';
 
+void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    tester.runAsync(() async{
-      when(valueRepository.getValues())
-        .thenAnswer((_) => Future.delayed(Duration(seconds: 30), ()=> []));
-    await tester.pumpWidget(MyApp(ValueListPage(
-      valueRepository: valueRepository,
-    )));
-    
-      await tester.pumpAndSettle(
-        Duration(seconds: 10), EnginePhase.build, Duration(minutes: 1));
-      
-      expect(find.byType(CircularProgressIndicator), findOneWidget);
-    
-  },);
-  },
-   );
+    await tester.pumpWidget(const MyApp());
+
+    // Verify that our counter starts at 0.
+    await tester pump();
+    expect(find.text('0'), findsOneWidget);
+    await tester pumpAndSettle();
+    expect(find.text('1'), findsNothing);
+
+    // Tap the '+' icon and trigger a frame.
+    await tester.tap(find.byIcon(Icons.add));
+    await tester.pump();
+
+    // Verify that our counter has incremented.
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
+  });
+}
+
