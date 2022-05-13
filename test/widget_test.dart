ca4c789a3f3001ce4049095f788f26e57340467b
Gudani55 <69737478+Gudani55@users.noreply.github.com>
2022-05-13 22:27:15 +0200

    Update widget_test.dart
    test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,18 +13,17 @@ import 'package:harvest/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    tester.runAsync(() async{
+      when(valueRepository.getValues())
+        .thenAnswer((_) => Future.delayed(Duration(seconds: 30), ()=> []));
+    await tester.pumpWidget(withApp(ValueListPage(
+      valueRepository: valueRepository,
+    )));
+    
+      await tester.pumpAndSettle(
+        Duration(seconds: 10), EnginePhase.build, Duration(minutes: 1));
+      
+      expect(find.byType(CircularProgressIndicator), findOneWidget);
+    
   });
 }
